c205a56480e774310422226e84719f249d3f4dbc
Juli G <55200452+julito97@users.noreply.github.com>
2022-06-26 17:06:27 +0200

    25-06
    17:06

---
+++ GameLib/app/src/main/java/com/cursoudemy/gamelib/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.cursoudemy.gamelib
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursoudemy.gamelib.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mBinding.root)
+        // cuando pulsamos iniciar sesión
+        mBinding.btnLoginMain.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        //cuando debemos registrarnos
+        mBinding.btnSignInMain.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
+
+
+/*
+* REQUERIDO: enviar el email a la siguiente actividad
+* */
